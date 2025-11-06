fe7a8036ede075b1bb2ad8f43c64b7ab6a77d2d2
saisravanthi-35 <bhagyasaisravanthi2002@gmail.com>
2025-11-06 15:03:25 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,78 @@
+"""list1 = ["hello","aditya"]
+list2 = [1,2,3]
+list3 = [1,"hi",2.9]
+list4 = [True,False,False]
+list5 = [2.3,4,5]
+print(list5)
+print(list5[2:])"""
+
+"""list3= [1,"hi",2.6,3,5,6]
+print(list3)
+list3[1] = "Hello"                 #slicing
+list3.insert(2, "Python")
+print(list3)
+print(len(list3))"""
+
+"""list1 = [1,2,3]
+list2 = [2.6,3,4]
+print(list1+list2)
+list1.extend(list2)
+print(list1)
+print(list2)
+list4 = list2.reverse()
+print(list2)
+list4 = list2.copy()
+print(list1)
+print(list1[2:])"""
+
+"""mylist= [3,6,8,1,2]
+mylist.sort()
+print(mylist)
+mylist2 = [4,2,5,1,8]
+mylist2.sort(reverse=True)
+print(mylist2)"""
+
+"""newlist = ["A","a","C""b","c"]
+l=len(newlist)
+for y in range(0,1-1,1):
+    print(newlist[y])
+
+for x in range(2,10,3):
+    print(x)"""
+
+glist = [10,2,40,5,14,23]
+if(g>list[i]):
+    print(list)
+
+
+"""if "C" in newlist:
+    print("yes")
+else:
+    print("No")"""
+
+"""newlist.sort(key = str.lower)
+print(newlist)
+newlist.append("z")              #append=add
+print(newlist) 
+newlist.remove("a")
+print(newlist) """
+
+"""newlist.pop()
+print(newlist)
+
+newlist.pop(0)
+print(newlist)
+
+del newlist[0]
+print(newlist)
+
+""'del newlist
+print(newlist)"""
+
+"""list = [3,4,6,8]
+list.clear()
+print(list)"""
+
+
+
+
